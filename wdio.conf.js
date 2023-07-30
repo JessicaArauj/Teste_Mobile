af5cf25263adb6828d183590e8237f92d597cf23
Jessica Teixeira Araujo <jessicateixeiraaraujo@gmail.com>
2023-07-30 15:05:16 -0300

    update

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,6 @@
-const {join} = require('path')
+const { join } = require('path')
+// const allure = require('allure-commandline')
+// const video = require('wdio-video-reporter');
 
 exports.config = {
     hostname: '127.0.0.1', // ip local
@@ -9,7 +11,7 @@ exports.config = {
         './test/specs/**/*spec.js'
     ],
     framework: 'mocha',
-    capabilities:[{
+    capabilities: [{
         "platformName": "Android",
         "platformVersion": "9.0",
         "deviceName": "ebac-qe",
@@ -23,4 +25,38 @@ exports.config = {
     mochaOpts: {
         timeout: 300000
     },
+    // reporters: ['spec',
+    //     [video, {
+    //         saveAllVideos: true,       // If true, also saves videos for successful test cases
+    //         videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
+    //     }],
+    //     ['allure', {
+    //         outputDir: 'allure-results',
+    //         disableWebdriverStepsReporting: true,
+    //         disableWebdriverScreenshotsReporting: true,
+    //     }],
+    // ],
+    // onComplete: function () {
+    //     const reportError = new Error('Could not generate Allure report')
+    //     const generation = allure(['generate', 'allure-results', '--clean'])
+    //     return new Promise((resolve, reject) => {
+    //         const generationTimeout = setTimeout(
+    //             () => reject(reportError),
+    //             5000)
+
+    //         generation.on('exit', function (exitCode) {
+    //             clearTimeout(generationTimeout)
+
+    //             if (exitCode !== 0) {
+    //                 return reject(reportError)
+    //             }
+
+    //             console.log('Allure report successfully generated')
+    //             resolve()
+    //         })
+    //     })
+    // },
+    // afterStep: async function (step, scenario, { error, duration, passed }, context) {
+    //     driver.takeScreenshot();
+    // }
 }
